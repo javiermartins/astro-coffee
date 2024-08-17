088206950cd31006fd5d2481a680db837bf19270
javiermartins <javiimartinserrano@gmail.com>
2024-08-17 13:51:26 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Central Perks
+
+Website made with Astro inspired by the website of a café-restaurant.
---
+++ src/types/desayunos.ts
@@ -0,0 +1,6 @@
+export type Desayunos = {
+    title?: string;
+    description?: string;
+    price?: string;
+    img?: string;
+  }
